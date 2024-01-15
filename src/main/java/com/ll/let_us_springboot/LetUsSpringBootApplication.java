ab226c11195e4da4cfe4bbd6434cf9414eef0b30
N1Alice <kc700370@gmail.com>
2024-01-15 17:31:18 +0900

    Setting

---
+++ src/main/java/com/ll/let_us_springboot/LetUsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.ll.let_us_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetUsSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetUsSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/let_us_springboot/LetUsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.let_us_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetUsSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
